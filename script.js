8092eba82aaf5db6e1b207a334505af5ac8b202c
O23404 <O23404@lab.aca>
2026-02-19 12:29:10 -0500

    prototipo IA

---
+++ script.js
@@ -0,0 +1,41 @@
+const URL = "https://teachablemachine.withgoogle.com/models/ovbVEoEWg/";
+
+let model, webcam, labelContainer, maxPredictions;
+
+async function init() {
+
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    webcam = new tmImage.Webcam(300, 300, true);
+    await webcam.setup();
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    labelContainer = document.getElementById("label-container");
+}
+
+async function loop() {
+    webcam.update();
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+async function predict() {
+    const prediction = await model.predict(webcam.canvas);
+
+    let highest = prediction.reduce((prev, current) =>
+        prev.probability > current.probability ? prev : current
+    );
+
+    labelContainer.innerHTML =
+        "Residuo detectado: " +
+        highest.className +
+        " (" +
+        (highest.probability * 100).toFixed(2) +
+        "%)";
+}
